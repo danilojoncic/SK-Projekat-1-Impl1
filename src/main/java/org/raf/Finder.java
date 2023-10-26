b8f8ba6b2c2077f8b31985a0e97f83a7924bdeaf
danilojoncic <djoncic14122rn@raf.rs>
2023-10-26 22:07:27 +0200

    init imp1 i maven dependency

---
+++ src/main/java/org/raf/Finder.java
@@ -0,0 +1,4 @@
+package org.raf;
+
+public class Finder {
+}
---
+++ src/main/java/org/raf/Implementation.java
@@ -0,0 +1,27 @@
+package org.raf;
+
+import model.Raspored;
+import model.Specifikacija;
+
+public class Implementation implements Specifikacija{
+
+    @Override
+    public void stampaj() {
+
+    }
+
+    @Override
+    public void kreirajRaspored() {
+
+    }
+
+    @Override
+    public void pretraziRaspred() {
+
+    }
+
+    @Override
+    public void sacuvajRaspored() {
+
+    }
+}
