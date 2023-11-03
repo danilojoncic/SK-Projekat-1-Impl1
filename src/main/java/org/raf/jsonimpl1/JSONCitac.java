6512a61a74741eedb1b8cd366873a9318e2d9c37
aaleksaaleksic <aaleksic9722@raf.rs>
2023-11-03 23:55:06 +0100

    radi json pisac

--- src/main/java/org/raf/jsonimpl1/JSONCitac.java
+++ src/main/java/org/raf/jsonimpl1/JSONCitac.java
@@ -1,4 +1,33 @@
 package org.raf.jsonimpl1;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.boljeRijesenje.Dogadjaj;
+import model.boljeRijesenje.Raspored;
+import org.raf.csvimpl1.CSVPisac;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public class JSONCitac {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            File file = new File("raspored.json"); // Ime JSON datoteke koju želite učitati
+
+            Raspored raspored = mapper.readValue(file,Raspored.class);
+
+
+
+
+
+            CSVPisac csvPisac = new CSVPisac();
+            csvPisac.napisi(raspored);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/raf/jsonimpl1/JSONPIsac.java
+++ src/main/java/org/raf/jsonimpl1/JSONPIsac.java
@@ -1,10 +1,13 @@
 package org.raf.jsonimpl1;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import model.boljeRijesenje.Dogadjaj;
 import model.boljeRijesenje.Raspored;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.raf.csvimpl1.CSVCitac;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,36 +16,55 @@ import java.util.List;
 
 public class JSONPIsac {
 
+
+
+
+
     public JSONPIsac() {
+
     }
 
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        CSVCitac csvCitac = new CSVCitac();
+        Raspored raspored = csvCitac.citaj("C:\\Users\\Korisnik\\Desktop\\csv.csv");
+//        String json = mapper.writeValueAsString(raspored);
 
+        File file = new File("raspored.json");
+        mapper.writeValue(file,raspored.getDogadjaji());
+
+    }
     public void napisiJSON(Raspored raspored){
 
-
-        List<JSONArray> nizJsona = new ArrayList<>();
-        List<Dogadjaj> dogadjaji = raspored.getDogadjaji();
-        for (Dogadjaj dogadjaj : dogadjaji) {
-            JSONArray jsonArray = new JSONArray();
-            for(String string : dogadjaj.getStavkeDogadjaja()){
-                jsonArray.put(string);
-            }
-            nizJsona.add(jsonArray);
-        }
-        String filePath = "output.json";
-
-        try {
-            FileWriter fileWriter = new FileWriter(filePath);
-            for(JSONArray jsonArray : nizJsona){
-                System.out.println(jsonArray.toString(4));
-                fileWriter.write(jsonArray.toString(1));
-                //fileWriter.write(jsonArray.toString(4));
-            }
-            fileWriter.close();
-            System.out.println("JSON data has been written to " + filePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
+
+
+//    public void napisiJSON(Raspored raspored){
+//
+//
+//        List<JSONArray> nizJsona = new ArrayList<>();
+//        List<Dogadjaj> dogadjaji = raspored.getDogadjaji();
+//        for (Dogadjaj dogadjaj : dogadjaji) {
+//            JSONArray jsonArray = new JSONArray();
+//            for(String string : dogadjaj.getStavkeDogadjaja()){
+//                jsonArray.put(string);
+//            }
+//            nizJsona.add(jsonArray);
+//        }
+//        String filePath = "output.json";
+//
+//        try {
+//            FileWriter fileWriter = new FileWriter(filePath);
+//            for(JSONArray jsonArray : nizJsona){
+//                System.out.println(jsonArray.toString(4));
+//                fileWriter.write(jsonArray.toString(1));
+//                //fileWriter.write(jsonArray.toString(4));
+//            }
+//            fileWriter.close();
+//            System.out.println("JSON data has been written to " + filePath);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
