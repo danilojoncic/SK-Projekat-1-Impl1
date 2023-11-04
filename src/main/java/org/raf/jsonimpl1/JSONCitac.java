99bdc0304dd5ebd254a939edca8c6b3740d8286e
aaleksaaleksic <aaleksic9722@raf.rs>
2023-11-04 16:20:56 +0100

    Uradjen JSON CITAC PISAC

--- src/main/java/org/raf/jsonimpl1/JSONCitac.java
+++ src/main/java/org/raf/jsonimpl1/JSONCitac.java
@@ -1,33 +1,80 @@
 package org.raf.jsonimpl1;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import model.boljeRijesenje.Dogadjaj;
 import model.boljeRijesenje.Raspored;
-import org.raf.csvimpl1.CSVPisac;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.raf.csvimpl1.CSVCitac;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class JSONCitac {
-    public static void main(String[] args) {
-        ObjectMapper mapper = new ObjectMapper();
 
-        try {
-            File file = new File("raspored.json"); // Ime JSON datoteke koju želite učitati
 
-            Raspored raspored = mapper.readValue(file,Raspored.class);
+    Raspored raspored;
+    String filePath = "raspored.json";  // Replace with your file path
+    Path path = Paths.get(filePath);
 
 
 
+    public JSONCitac(Raspored raspored) {
+        this.raspored = raspored;
 
-
-            CSVPisac csvPisac = new CSVPisac();
-            csvPisac.napisi(raspored);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
+
+    public void ispisiJSON(File file) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String sadrzaj = new String(Files.readAllBytes(path));
+
+        Gson gson = new Gson();
+
+        Type listType = new TypeToken<List<List<String>>>() {}.getType();
+        List<List<String>> listOfLists = gson.fromJson(sadrzaj,listType);
+
+        Raspored raspored = new Raspored();
+        for (List<String> listOfList : listOfLists) {
+            Dogadjaj d = new Dogadjaj(listOfList);
+            raspored.getDogadjaji().add(d);
+        }
+
+        raspored.refresh(raspored.getDogadjaji());
+
+
+
+
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        CSVCitac csvCitac = new CSVCitac();
+        Raspored raspored = csvCitac.citaj("C:\\Users\\Korisnik\\Desktop\\csv.csv");
+        File file = new File("raspored.json");
+
+        JSONPIsac jsonpIsac = new JSONPIsac(raspored);
+        jsonpIsac.ispisiJSON(file);
+
+        for (Dogadjaj d : raspored.getDogadjaji()) {
+            System.out.println(d);
+        }
+
+    }
+
+
+
+
 }
+
--- src/main/java/org/raf/jsonimpl1/JSONPIsac.java
+++ src/main/java/org/raf/jsonimpl1/JSONPIsac.java
@@ -1,15 +1,20 @@
 package org.raf.jsonimpl1;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import model.boljeRijesenje.Dogadjaj;
 import model.boljeRijesenje.Raspored;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.raf.csvimpl1.CSVCitac;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -17,54 +22,48 @@ import java.util.List;
 public class JSONPIsac {
 
 
+    Raspored raspored;
+    String filePath = "raspored.json";  // Replace with your file path
+    Path path = Paths.get(filePath);
 
 
 
-    public JSONPIsac() {
+    public JSONPIsac(Raspored raspored) {
+        this.raspored = raspored;
 
     }
+
+    public void ispisiJSON(File file) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<String> header = raspored.getHeader().getStavkeDogadjaja();
+        List<List<String>> listaDogadjaja = new ArrayList<>();
+        listaDogadjaja.add(header);
+        for (Dogadjaj dogadjaj : raspored.getDogadjaji()) {
+            List<String> d = dogadjaj.getStavkeDogadjaja();
+            listaDogadjaja.add(d);
+        }
+
+        objectMapper.writeValue(file,listaDogadjaja);
+
+    }
+
 
     public static void main(String[] args) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
         CSVCitac csvCitac = new CSVCitac();
         Raspored raspored = csvCitac.citaj("C:\\Users\\Korisnik\\Desktop\\csv.csv");
-//        String json = mapper.writeValueAsString(raspored);
-
         File file = new File("raspored.json");
-        mapper.writeValue(file,raspored.getDogadjaji());
+
+        JSONPIsac jsonpIsac = new JSONPIsac(raspored);
+        jsonpIsac.ispisiJSON(file);
+
+        for (Dogadjaj d : raspored.getDogadjaji()) {
+            System.out.println(d);
+        }
 
     }
-    public void napisiJSON(Raspored raspored){
-
-    }
 
 
-//    public void napisiJSON(Raspored raspored){
-//
-//
-//        List<JSONArray> nizJsona = new ArrayList<>();
-//        List<Dogadjaj> dogadjaji = raspored.getDogadjaji();
-//        for (Dogadjaj dogadjaj : dogadjaji) {
-//            JSONArray jsonArray = new JSONArray();
-//            for(String string : dogadjaj.getStavkeDogadjaja()){
-//                jsonArray.put(string);
-//            }
-//            nizJsona.add(jsonArray);
-//        }
-//        String filePath = "output.json";
-//
-//        try {
-//            FileWriter fileWriter = new FileWriter(filePath);
-//            for(JSONArray jsonArray : nizJsona){
-//                System.out.println(jsonArray.toString(4));
-//                fileWriter.write(jsonArray.toString(1));
-//                //fileWriter.write(jsonArray.toString(4));
-//            }
-//            fileWriter.close();
-//            System.out.println("JSON data has been written to " + filePath);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+
 
 }
